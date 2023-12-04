db3a0ebfeaaf150991a2f4d982edfd17f9e931d7
karim831 <112541594+karim831@users.noreply.github.com>
2023-12-04 20:21:57 +0200

    Update number_of_1's.c

--- task4/Q4/number_of_1's.c
+++ task4/Q4/number_of_1's.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main(){
+    printf("enter the number :");
+    unsigned int number;
+    scanf("%u",&number);
+    int shifting = 0,counter =0;
+    while(shifting<32){
+        if(((number>>shifting++)&1) == 1)
+            counter++;
+    }
+    printf("%d",counter);
+}
