8d2d1f12eb9a06d577ee57601ef89a946bdf6e5d
karim831 <112541594+karim831@users.noreply.github.com>
2023-12-04 20:22:52 +0200

    Update binary_representation.c

--- task4/Q3/binary_representation.c
+++ task4/Q3/binary_representation.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main(){
+    printf("entet the number:");
+    int num;
+    scanf("%d",&num);
+    int shifting = 32;
+    if(num >=0)
+        while((num&(1<<--shifting)) == 0);
+    else{
+        while((((unsigned int)(num&(1<<--shifting)))>>shifting) == 1 && shifting>=0);
+        shifting++;
+    }
+    while(shifting>=0)
+        printf("%d",(num&(1<<shifting))>>shifting--);
 
+}
