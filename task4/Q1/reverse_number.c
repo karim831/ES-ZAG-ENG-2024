9482a9dec6c055e98796f3894981259142fcb508
karim831 <112541594+karim831@users.noreply.github.com>
2023-12-04 18:18:07 +0200

    Update reverse_number.c

--- task4/Q1/reverse_number.c
+++ task4/Q1/reverse_number.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+int main(){
+    printf("Enter your number to reverse : ");
+    int num;
+    scanf("%d",&num);
+    int ret = 0;
+    while(num!=0){
+        ret*=10;
+        ret+= num%10;
+        num/=10;
+    }    
+    printf("reverse number is %d",ret);
+}
