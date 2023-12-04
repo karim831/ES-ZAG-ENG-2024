d0f7b3c54cdb926031db5c2e666292a72d892249
karim831 <112541594+karim831@users.noreply.github.com>
2023-12-04 21:05:05 +0200

    Update ld_of_multplication.c

--- task4/Q6/ld_of_multplication.c
+++ task4/Q6/ld_of_multplication.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main(){
+    int a,b,c,d;
+    printf("Input ");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    int mul = a*b*c*d,tmp = mul%100;
+    if(tmp < 10)
+        printf("%d%d",0,tmp);
+    else
+        printf("%d",tmp);
+}
