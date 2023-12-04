f8055119188a69c3d3a58cdc98ebadfe463fc80e
karim831 <112541594+karim831@users.noreply.github.com>
2023-12-04 18:56:46 +0200

    Update case_of_character.c

--- task4/Q2/case_of_character.c
+++ task4/Q2/case_of_character.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+    printf("enter an alphapetical character and a number respectivly:");
+    char character;
+    int num;
+    scanf("%c %d",&character,&num);
+    if(num%2 != 0){
+        if(character >='a' && character <='z')
+            character-=('a'-'A');
+        else if(character >='A' && character <= 'Z')
+            character+=('a'-'A');
+    }
+    printf("Character is %c",character);
+}
