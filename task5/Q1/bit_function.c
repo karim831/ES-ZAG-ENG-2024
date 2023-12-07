9e34a4ee1cdab70d5442f960efc3e6bd1bf02cc3
karim831 <112541594+karim831@users.noreply.github.com>
2023-12-07 20:07:49 +0200

    Update bit_function.c

--- task5/Q1/bit_function.c
+++ task5/Q1/bit_function.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 unsigned char set(unsigned char num,unsigned char bit_number){
-    return num |(1<<(bit_number-1));
+    return num |(1<<bit_number);
 }
 unsigned char clear(unsigned char num,unsigned char bit_number){
-    return num & (~(1<<bit_number-1));
+    return num & (~(1<<bit_number));
 }
 unsigned char toggle(unsigned char num , unsigned char bit_number){
-    return num ^ (1<<(bit_number-1));
+    return num ^ (1<<bit_number);
 }
 int main(){
     printf("input\n");
     unsigned char num,bit_number,function_id;
     scanf("%hhu%hhu%hhu",&num,&bit_number,&function_id);
     printf("output\n");
-    if(function_id == 1)
-        printf("%hhu\n",set(num,bit_number));
-    else if(function_id == 2)
-        printf("%hhu\n",clear(num,bit_number));
-    else if(function_id == 3)
-        printf("%hhu\n",toggle(num,bit_number));
-    else
-        printf("%hhu\n",num);
+    switch(function_id){
+        case 1:
+            printf("%hhu\n",set(num,bit_number));
+            break;
+        case 2:
+            printf("%hhu\n",clear(num,bit_number));
+            break;
+        case 3:
+            printf("%hhu\n",toggle(num,bit_number));
+            break;
+        default:
+            printf("%hhu\n",num);
+    }
 }
