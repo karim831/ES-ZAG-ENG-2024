6db712d70cb7ccca42fbd972308b7ef57e244921
karim <karim@gmail.com>
2024-02-06 18:21:21 +0200

    Q2 finished

---
+++ task8/Q2/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(){
+    printf(MY_NAME);
+    return 0;
+}
---
+++ task8/Q2/main.h
@@ -0,0 +1,4 @@
+#ifndef _MAIN_H_
+    #define _MAIN_H_
+    #define MY_NAME "karim osama"
+#endif
